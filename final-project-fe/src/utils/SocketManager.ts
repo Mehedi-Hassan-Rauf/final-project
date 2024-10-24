9fb3b934657a19450a58d93549eb6db5b83922d4
Rauf <raufnx@gmail.com>
2024-10-24 21:14:40 +0600

    url added

--- final-project-fe/src/utils/SocketManager.ts
+++ final-project-fe/src/utils/SocketManager.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // initializing the socket connection
-let socket = io("https://final-project-be-five.vercel.app");
+let socket = io("https://final-project-2vdl.onrender.com");
 
 export default socket;
--- final-project-fe/src/utils/axios.ts
+++ final-project-fe/src/utils/axios.ts
@@ -12,7 +12,7 @@ const useAxios = () => {
 
   // method to handle all type of api request ( GET, POST, PUT, DELETE )
   const HttpRequestController = async (path: string, method = HTTP_METHODS.GET, payload: any = null) => {
-    let url = 'https://final-project-be-five.vercel.app' + path;
+    let url = 'https://final-project-2vdl.onrender.com' + path;
 
     // headers are set here, retrieves the jwt token from local storage 
     let headers = {
